689b8458ce33e9523733d5e03286435c21694c1c
Opixelum <48217573+opixelum@users.noreply.github.com>
2023-08-23 11:04:59 +0200

    Add a diamond (#4)
    * forge install: solidstate-solidity
    
    v0.0.58
    
    * Add remappings
    
    * Specify solc version in `foundry.toml`
    
    * Add `NotarizationDiamond.sol`

---
+++ src/NotarizationDiamond.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.21;
+
+import {SolidStateDiamond} from "solidstate-solidity/proxy/diamond/SolidStateDiamond.sol";
+
+contract NotarizationDiamond is SolidStateDiamond {}
