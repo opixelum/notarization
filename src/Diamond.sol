fee350b600e51bf87f99a7323ca4f72cbeddba81
opixelum <48217573+opixelum@users.noreply.github.com>
2023-08-23 11:42:35 +0200

    Rename diamond

--- src/Diamond.sol
+++ src/Diamond.sol
@@ -3,4 +3,4 @@ pragma solidity ^0.8.21;
 
 import {SolidStateDiamond} from "solidstate-solidity/proxy/diamond/SolidStateDiamond.sol";
 
-contract NotarizationDiamond is SolidStateDiamond {}
+contract Diamond is SolidStateDiamond {}
